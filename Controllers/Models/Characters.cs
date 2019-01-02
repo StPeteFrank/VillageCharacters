c3bef4552d54c94c9f0cb12a9ac94ce42698ea67
Frank Diaz <diazfrankp@gmail.com>
2019-01-02 18:42:44 -0500

    Created intial tables Villages.cs and Characters.cs.

---
+++ Controllers/Models/Characters.cs
@@ -0,0 +1,15 @@
+namespace villagecharacters.Models
+{
+  class Characters
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Health { get; set; }
+
+    public string Speed { get; set; }
+
+    public string Hitpoints { get; set; }
+  }
+}
---
+++ Controllers/Models/Villages.cs
@@ -0,0 +1,15 @@
+namespace villagecharacters.Models
+{
+  class Villages
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string VillageLevel { get; set; }
+
+    public string Capacity { get; set; }
+
+    public string NumberOfWalls { get; set; }
+  }
+}
