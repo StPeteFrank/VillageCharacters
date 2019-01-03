a57ca29ec593777837e77fb7e92d8495bd28ec33
Frank Diaz <diazfrankp@gmail.com>
2019-01-03 13:55:59 -0500

    Initial Controlllers templates.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using villagecharacters.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace carsearchapi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CharacterController : ControllerBase
+  {
+
+
+  }
+}
---
+++ Controllers/VillageController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using villagecharacters.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace carsearchapi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class VillageController : ControllerBase
+  {
+
+
+
+  }
+}
